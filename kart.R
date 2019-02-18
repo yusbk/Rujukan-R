f9c62d91bc6699a4cf838d054039257cf96f4e07
Yusman BK <ybkamaleri@gmail.com>
2019-02-18 01:05:05 +0100

    kart av norge

---
+++ kart.R
@@ -0,0 +1,79 @@
+list.of.packages <- c("maps","mapdata","maptools","rgdal","ggmap","leaflet","tigris",
+                      "sp","ggplot2","plyr","animation","gridExtra","psych","rstudioapi",
+                      "data.table")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+library(geojsonR)
+
+nokart <- geojsonR::FROM_GeoJson(url_file_string = "~/Documents/kart/Basisdata_0000_Norge_25833_Kommuner_GEOJSON.geojson")
+
+## --------
+library(maps)
+library(mapdata)
+library(maptools)
+library(rgdal)
+library(ggmap)
+library(leaflet)
+library(tigris)
+library(sp)
+library(ggplot2)
+library(plyr)
+library(animation)
+library(gridExtra)
+library(psych)
+library(rstudioapi)
+library(data.table)
+
+## Sjekk forskjellige driver som kan brukes
+ogrDrivers()$name
+
+## Bruk av GeoJSON fil
+## Sjekk layer
+ogrListLayers(dsn = "~/Documents/kart/Basisdata_0000_Norge_25833_Kommuner_GEOJSON.geojson")
+norgeKart <- rgdal::readOGR(dsn = "~/Documents/kart/Basisdata_0000_Norge_25833_Kommuner_GEOJSON.geojson",
+  layer = "administrative_enheter.avtaltavgrensningslinje")
+
+plot(norgeKart)
+
+
+## Bruk av Shapefile
+library(rgdal)
+ogrListLayers(dsn = "~/Documents/kart/norge")
+norgeSF <- rgdal::readOGR(dsn = "~/Documents/kart/norge", layer = "NOR_adm1", encoding = "UTF-8")
+plot(norgeSF)
+
+library(broom)
+library(rgeos)
+library(maptools)
+str(norgeSF@data)
+norgeDF <- broom::tidy(norgeSF, region = "NAME_1")
+head(norgeDF)
+
+## hvis region ikke funker for 'tidy' er fordi pakke maptools, rgeos, eller broom ikke er installert.
+## Hvis man ikke kan installere pakke 'rgeos' kan være at man må først installere
+## sudo apt-get install libgeos++-dev
+
+fylke <- data.frame(norgeSF@data$NAME_1)
+names(fylke) <- "fylke"
+fylke
+fylke$id <- seq(0, nrow(fylke) - 1)
+
+library(data.table)
+norgeDT <- as.data.table(norgeDF)norgeDT[, unique(id)]
+
+library(ggplot2)
+ggplot(data = norgeDT, aes(long, lat, map_id = id)) +
+  geom_map(aes(map_id = id, fill = as.factor(id)), map = norgeDT) +
+  coord_map()
+
+
+fylkeid <- norgeDT[, .(id = unique(id))]
+fylkeid[, tall := sample(30:95, 19, replace = T)]
+
+norgeDD <- fylkeid[norgeDT, on = "id"]
+dim(norgeDD)
+
+ggplot(data = norgeDD, aes(long, lat, map_id = id)) +
+  geom_map(data = norgeDD, aes(map_id = id, fill = tall), map = norgeDD) +
+  coord_map()
