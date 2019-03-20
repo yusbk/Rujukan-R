dd3d295057aef9947b84b0cd9ff974dfbaac85ef
Yusman BK <ybkamaleri@gmail.com>
2019-03-20 18:13:34 +0100

    update

--- kart/norgekart_sf.R
+++ kart/norgekart_sf.R
@@ -6,20 +6,72 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 sapply(list.of.packages, require, character.only = TRUE)
 
+#kart fylke
 kart_sf = sf::st_read("~/Git-personal/Rujukan-R/norge/NOR_adm1.shp", stringsAsFactors = FALSE)
+## kart kommune
+kart_sf2 = sf::st_read("~/Git-personal/Rujukan-R/norge/NOR_adm2.shp", stringsAsFactors = FALSE)
 class(kart_sf)
+
+swed_sf = sf::st_read("~/Git-personal/kart/sweden/se_10km.shp", stringsAsFactors = FALSE)
+plot(st_geometry(swed_sf))
 
 ## plot(kart_sf) #ikke kjør. Denne lages 6 kart
 plot(st_geometry(kart_sf)) #bruk denne for å plotte et kart
+plot(st_geometry(kart_sf2)) #bruk denne for å plotte et kart på kommune
 
 ## Se inhold og struktur
 st_geometry(kart_sf)
 head(kart_sf)
+st_geometry(kart_sf2)
+kart_sf2
+dim(kart_sf2)
 
 ## bytte Åstfold to Østfold
 library(dplyr)
 kart_sf <- kart_sf %>%
   mutate(NAME_1, fylke=ifelse(ID_1==1, 'Østfold', NAME_1))
+
+nrow(kart_sf)
+
+kart_sf$fylke
+
+hso <- c("Østfold", "Akershus", "Aust-Agder", "Buskerud", "Hedmark", "Oppland", "Oslo", "Telemark", "Vest-Agder", "Vestfold")
+hsv <- c("Hordaland", "Rogaland", "Sogn og Fjordane")
+hsm <- c("Møre og Romsdal", "Sør-Trøndelag", "Nord-Trøndelag")
+hsn <- c("Finnmark", "Nordland", "Troms")
+
+hso1 <- c()
+
+
+kart_sf <- kart_sf %>%
+  mutate(fylke, helsereg = ifelse(fylke %in% hso, 1, ifelse(fylke %in% hsv, 2, ifelse(fylke %in% hsm, 3, 4))))
+
+## dekningsgrad BDR
+kart_sf <- kart_sf %>%
+  mutate(helsereg, helsenavn =
+                     ifelse(fylke == 1, "Helse Sør-Øst",
+                       ifelse(fylke == 2, "Helse Vest",
+                         ifelse(fylke == 3, "Helse Midt", "Helse Nord")))) %>%
+  mutate(helsereg, dekning =
+                     ifelse(fylke == 1, 99,
+                       ifelse(fylke == 2, 98,
+                         ifelse(fylke == 3, 98, 97))))
+
+library(plotly)
+## tooltips ved hoving
+plot_ly(
+  kart_sf,
+  split = ~fylke, #for at hoveron blir categorisert riktig så må bruke 'Split'
+  color = ~helsereg,
+    text = ~paste("Dekningsgrad ", dekning),
+  hoverinfo = "text",
+  hoveron = "fill" #brukes for å vise utvalgte tooltips annen enn district
+)
+
+kart_sf
+
+
+
 
 ## Hvis det er behov å konvertere sf til data.frame
 ## bruk st_geometry
@@ -27,12 +79,24 @@ kart_df <- kart_sf
 class(kart_df) #class sf og data.frame
 sf::st_geometry(kart_df) <- NULL
 class(kart_df) #class data.frame
-
+str(bodo)
 
 ## Simple kart
 library(ggplot2)
-g1 <- ggplot(kart_sf, aes(fill=fylke)) + geom_sf()
+g1 <- ggplot(kart_sf, aes(fill=as.factor(helsereg))) + geom_sf()
+g1 + geom_point(data = bodo, mapping = aes(lon, lan)) + coord_sf()
+g1 + theme(legend.position = 'none')
 g1
+
+bodo  <- data.frame(navn = "Bodø", lan = 67.2804, lon = 14.4049)
+ggplot(kart_sf) + geom_sf() +
+  geom_point(data = bodo, mapping = aes(lon, lan), size = 4, shape = 20, color = "red") +
+  geom_text(data = bodo, mapping = aes(lon, lan, label = navn), nudge_x = -2) +
+  coord_sf()
+
+bodo
+plotg1 <- plotly::ggplotly(g1)
+
 
 ## Bruk av forskjellige Coordinate Reference System
 ## F.eks 4326 og 2154
@@ -114,3 +178,114 @@ fylkeco
 valgFylke <- subset(kart_sf, ID_1 %in% c(12, 10, 14, 7))
 plot(st_geometry(kart_sf))
 plot(st_geometry(valgFylke), add = TRUE, col = "red")
+
+
+##############################
+## plotly style
+library(plotly)
+library(ggplot2)
+
+packageVersion("plotly")
+
+
+nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
+
+nc
+class(nc)
+
+p <- plot_ly(nc)
+p
+
+# Create a shareable link to your chart
+# Set up API credentials: https://plot.ly/r/getting-started
+chart_link = api_create(p, filename="sf-1")
+chart_link
+
+p <- plot_geo(nc)
+p
+# Create a shareable link to your chart
+# Set up API credentials: https://plot.ly/r/getting-started
+chart_link = api_create(p, filename="sf-geo")
+chart_link
+
+library(plotly)
+library(sf)
+
+nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
+
+p <- plot_mapbox(nc, split=~NAME) %>%
+  layout(
+    mapbox = list(
+      zoom = 6
+    )
+  )
+
+# Create a shareable link to your chart
+# Set up API credentials: https://plot.ly/r/getting-started
+chart_link = api_create(p, filename="sf-mapbox")
+chart_link
+
+## 3D testVirtual
+demo("sf-plotly-3D-globe", package = "plotly")
+
+## litt mer avansert
+## #################
+# load trails data (an sf object bundled with the mapview package)
+# install.packages('mapview')
+data(franconia, package = "mapview")
+franconia
+
+# Compare this result with: `plot_ly(franconia, split = ~NAME_ASCI, color = I("black"))`
+plot_ly(franconia, split = ~NAME_ASCI)
+plot_ly(franconia, split = ~NAME_ASCI, color = I("black"))#alt NAME_ASCI blir svart
+
+plot_ly(
+  franconia,
+  split = ~NUTS_ID, #trenger fordi color 'SHAPE_AREA' er nummerisk for at det ikke bare bli et type farge.
+  color = ~SHAPE_AREA,
+  alpha = 1,
+  showlegend = FALSE
+)
+
+## tooltips ved hoving
+plot_ly(
+  franconia,
+  split = ~NUTS_ID, #for at hoveron blir categorisert riktig så må bruke 'Split'
+  color = ~district,
+  stroke = I("black"),
+  text = ~paste(NAME_ASCI, "\n is in", district),
+  hoverinfo = "text",
+  hoveron = "fill" #brukes for å vise utvalgte tooltips annen enn district
+)
+
+## #####################
+## interaktiv Norge
+## ###################
+
+kart_sf
+nok1 <- plot_ly(kart_sf, split = ~fylke)
+nok1
+
+nok2 <- plot_geo(kart_sf)
+nok2
+
+nok3 <- plot_mapbox(kart_sf, split=~fylke) %>%
+  layout(
+    mapbox = list(
+      zoom = 6
+    )
+  )
+
+nok3
+
+
+cols <- c("#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7", "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD", "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D", "#8A7C64", "#599861")
+
+plot_ly(kart_sf, color = ~fylke, colors = cols)
+
+## Check json info
+nok1
+plotly_json(nok1)
+schema()
+
+plotly_json(plotg1)
