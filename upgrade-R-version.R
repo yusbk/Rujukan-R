f7088bbfac702965f40c4d2699a2067067b4557e
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2026-05-18 14:05:05 +0200

    Add R version to file for installed packages

--- upgrade-R-version.R
+++ upgrade-R-version.R
@@ -1,9 +1,9 @@
 ## Run in older version of R to save the list of installed packages before upgrading to a new version of R
 old_packages <- installed.packages()[, "Package"]
-saveRDS(old_packages, "packages.rds")
+saveRDS(old_packages, "packages4-5-1.rds")
 
 ## Run in new version of R to install the same packages as in the old version
-packages <- readRDS("packages.rds")
+packages <- readRDS("packages4-5-1.rds")
 install.packages(packages)
 
 ## Alternatively, use renv to manage package versions across R versions
